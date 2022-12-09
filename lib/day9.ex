43a1f2a995bfec67589a114da50dd67bae2d7680
John Ledbetter <john.ledbetter@grayscaleapp.com>
2022-12-09 10:41:05 -0500

    d9p2

--- lib/day9.ex
+++ lib/day9.ex
@@ -8,10 +8,21 @@ defmodule Day9 do
     Enum.reduce(commands, state, &evaluate_command/2)
   end
 
+  def evaluate_p2(commands) do
+    state = %{ head: { 0, 0 }, tails: [{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0}], visited: MapSet.new([{0, 0}]) }
+    Enum.reduce(commands, state, &evaluate_command_p2/2)
+  end
+
   defp evaluate_command({dx, dy, count}, state) do
     Range.new(1, count)
     |> Enum.reduce(state, fn _, accum -> move_head({dx, dy}, accum) end)
   end
+
+  defp evaluate_command_p2({dx, dy, count}, state) do
+    Range.new(1, count)
+    |> Enum.reduce(state, fn _, accum -> move_head_p2({dx, dy}, accum) end)
+  end
+
 
   defp move_head({dx, dy}, %{ head: {hx, hy}, tail: {tx, ty}, visited: visited }) do
     #IO.puts("head is at #{hx},#{hy}, tail is at #{tx},#{ty}")
@@ -21,6 +32,27 @@ defmodule Day9 do
     tail = move_tail({tx, ty}, diff)
     %{ head: {hx, hy}, tail: tail, visited: MapSet.put(visited, tail) }
   end
+
+  defp move_head_p2({dx, dy}, %{ head: {hx, hy}, tails: tails, visited: visited }) do
+    #IO.puts("head is at #{hx},#{hy}, tail is at #{tx},#{ty}")
+    {hx, hy} = {hx + dx, hy + dy}
+
+    tails = Enum.reduce(tails, [], fn {tx, ty}, new_tails ->
+      {px, py} = if length(new_tails) == 0 do
+          {hx, hy}
+        else
+          List.first(new_tails)
+        end
+
+      diff = {px - tx, py - ty}
+      t = move_tail({tx, ty}, diff)
+      [t | new_tails]
+    end)
+    #IO.inspect(List.first(tails))
+    
+    %{ head: {hx, hy}, tails: Enum.reverse(tails), visited: MapSet.put(visited, List.first(tails)) }
+  end
+
 
   defp move_tail(tail, {dx, dy}) when abs(dx) <= 1 and abs(dy) <= 1, do: tail
   defp move_tail({tx, ty}, {dx, dy}) do
--- test/day9_test.exs
+++ test/day9_test.exs
@@ -22,11 +22,41 @@ R 2
     assert MapSet.size(rc.visited) == 13
   end
 
+
+  @sample_p2 """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+
+  test "part 2 example" do
+    cmds = @sample_p2
+    |> String.trim
+    |> String.split("\n")
+    |> Day9.parse
+
+    rc = Day9.evaluate_p2(cmds)
+    assert MapSet.size(rc.visited) == 36
+  end
+
   test "part 1 solution" do
     cmds = input_lines(9)
     |> Day9.parse
 
     rc = Day9.evaluate(cmds)
     assert MapSet.size(rc.visited) == 5_779
+  end
+
+  test "part 2 solution" do
+    cmds = input_lines(9)
+    |> Day9.parse
+
+    rc = Day9.evaluate_p2(cmds)
+    assert MapSet.size(rc.visited) == 2_331
   end
 end
